9ccd8f8e823b19bece090d3e21670ca8eae5ac38
Joel Wanjala <joel.n.wanjala@gmail.com>
2022-09-27 09:42:14 +0300

    Added the functionality of the form and media queries

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const form = document.querySelector('.header-form');
+const input = document.querySelector('.header-input');
+const errorMsg = document.querySelector('.error-msg');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  if (input.value === '') errorMsg.style.opacity = 1;
+
+  input.focus();
+
+  setTimeout(function () {
+    errorMsg.style.opacity = 0;
+  }, 4000);
+});
